879706e9eb713f50dabca32b7f82f6953e022b43
“sethuaung” <sethuaung@outlook.com>
2025-03-27 18:20:26 +0630

    added js css

---
+++ js/contributors.js
@@ -0,0 +1,17 @@
+const contributors = [
+    {
+        name: 'jcubic',
+        fullname: 'Jakub Jankiewicz',
+        url: 'https://jcubic.pl/me'
+    },
+    {
+        fullname: 'Se Thu Aung',
+        name: 'sethuaung',
+        url: 'http://github.com/sethuaung'
+    },
+    {
+        fullname: 'Felixent',
+        name: 'felixent',
+        url: 'http://github.com/felixent
+    }
+];
